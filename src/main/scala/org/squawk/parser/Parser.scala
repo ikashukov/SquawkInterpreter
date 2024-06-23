0f49566ed3a47ef20151eb22d02af4c1ff94c430
Ivan Kashukov <ivan.kashukov@gmail.com>
2024-06-23 20:26:28 +0300

    moving error handling from Java exceptions to Either

--- src/main/scala/org/squawk/parser/Parser.scala
+++ src/main/scala/org/squawk/parser/Parser.scala
@@ -5,51 +5,60 @@ import tokens._
 import ast._
 
 object Parser {
-  def parse(tokens: List[Token]): Program = {
-    val (program, _) = parseProgram(tokens)
-    program
+  def parse(tokens: List[Token]): Either[String, Program] = {
+    parseProgram(tokens).map(_._1)
   }
 
-  private def parseProgram(tokens: List[Token]): (Program, List[Token]) = {
-    val (statements, remainingTokens) = parseStatements(tokens)
-    (Program(statements), remainingTokens)
+  private def parseProgram(tokens: List[Token]): Either[String, (Program, List[Token])] = {
+    parseStatements(tokens).map { case (statements, remainingTokens) =>
+      (Program(statements), remainingTokens)
+    }
   }
 
-  private def parseStatements(tokens: List[Token]): (List[Statement], List[Token]) = {
+  private def parseStatements(tokens: List[Token]): Either[String, (List[Statement], List[Token])] = {
     var remainingTokens = tokens
     var statements = List.empty[Statement]
 
-    while (remainingTokens.nonEmpty) {
-      val (statement, rest) = parseStatement(remainingTokens)
-      statements ::= statement
-      remainingTokens = rest
+    while (remainingTokens.nonEmpty && remainingTokens.head != CloseBracket) {
+      parseStatement(remainingTokens) match {
+        case Right((statement, rest)) =>
+          statements ::= statement
+          remainingTokens = rest
+        case Left(error) => return Left(error)
+      }
     }
-    (statements.reverse, remainingTokens)
+    Right((statements.reverse, remainingTokens))
   }
 
-  private def parseStatement(tokens: List[Token]): (Statement, List[Token]) = {
+  private def parseStatement(tokens: List[Token]): Either[String, (Statement, List[Token])] = {
     tokens match {
+      
       case Let :: Identifier(name) :: Assign :: rest =>
-        val (expr, afterExpr) = parseExpression(rest)
-        afterExpr match {
-          case Semicolon :: remainingTokens =>
-            (LetStmt(IdentifierExpr(name), expr), remainingTokens)
-          case _ => throw new RuntimeException("Expected ';' after let statement")
+        parseExpression(rest).flatMap { case (expr, afterExpr) =>
+          afterExpr match {
+            case Semicolon :: remainingTokens =>
+              Right((LetStmt(IdentifierExpr(name), expr), remainingTokens))
+            case _ => Left("Expected ';' after let statement")
+          }
         }
+        
       case _ =>
-        val (expr, remainingTokens) = parseExpression(tokens)
-        remainingTokens match {
-          case Semicolon :: rest => (ExpressionStmt(expr), rest)
-          case _ => throw new RuntimeException("Expected ';' after expression")
+        parseExpression(tokens).flatMap { case (expr, remainingTokens) =>
+          remainingTokens match {
+            case Semicolon :: rest => Right((ExpressionStmt(expr), rest))
+            case _ => Left("Expected ';' after expression")
+          }
         }
     }
   }
 
-  private def parseExpression(tokens: List[Token]): (Expression, List[Token]) = {
+  private def parseExpression(tokens: List[Token]): Either[String, (Expression, List[Token])] = {
     tokens match {
-      case Number(num) :: remainingTokens => (NumberLiteralExpr(num), remainingTokens)
-      case _ => throw new RuntimeException("Unsupported expression")
+      case Number(num) :: remainingTokens => Right((NumberLiteralExpr(num), remainingTokens))
+      case Identifier(name) :: remainingTokens => Right((IdentifierExpr(name), remainingTokens))
+      case True :: remainingTokens => Right((BooleanLiteralExpr(true), remainingTokens))
+      case False :: remainingTokens => Right((BooleanLiteralExpr(false), remainingTokens))
+      case _ => Left("Unsupported expression")
     }
   }
-
 }
--- src/test/scala/org/squawk/parser/ParserTest.scala
+++ src/test/scala/org/squawk/parser/ParserTest.scala
@@ -2,19 +2,33 @@ package org.squawk.parser
 
 import munit.FunSuite
 import org.squawk.ast.{IdentifierExpr, LetStmt, NumberLiteralExpr, Program}
-import org.squawk.tokens.*
+import org.squawk.lexer.Lexer
 
 class ParserTest extends FunSuite {
 
   test("'let' expression parsing test") {
 
-    // let a = 10;
-    val tokens = List(Let, Identifier("a"), Assign, Number(10), Semicolon)
+    val tokens = Lexer.tokenize("let a = 10;")
 
     val parsedProgram = Parser.parse(tokens)
     val expectedProgram = Program(List(LetStmt(IdentifierExpr("a"), NumberLiteralExpr(10))))
 
-    assertEquals(parsedProgram, expectedProgram)
+    parsedProgram match {
+      case Right(program) => assertEquals(program, expectedProgram)
+      case Left(error) => fail(s"Parsing failed with error: $error")
+    }
+  }
+
+  test("Invalid 'let' statement should fail") {
+
+    val tokens = Lexer.tokenize("let a 10;")
+
+    val parsedProgram = Parser.parse(tokens)
+
+    parsedProgram match {
+      case Right(_) => fail("Parsing should have failed")
+      case Left(error) => assertEquals(error, "Unsupported expression")
+    }
   }
 
 }
