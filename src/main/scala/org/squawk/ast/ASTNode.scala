a7b844d723bffa6706dfe2b9683d7909230ef8de
Ivan Kashukov <ivan.kashukov@gmail.com>
2024-06-25 22:50:36 +0300

    readability

--- src/main/scala/org/squawk/ast/ASTNode.scala
+++ src/main/scala/org/squawk/ast/ASTNode.scala
@@ -12,13 +12,10 @@ case class Program(statements: List[Statement]) extends ASTNode
 sealed trait Expression extends ASTNode
 
 case class IdentifierExpr(name: String) extends Expression
-
 case class NumberLiteralExpr(value: Int) extends Expression
-
 case class BooleanLiteralExpr(value: Boolean) extends Expression
 
 case class BinaryExpr(operator: Token, left: Expression, right: Expression) extends Expression
-
 case class UnaryExpr(operator: Token, operand: Expression) extends Expression
 
 case class FunctionCallExpr(function: Expression, arguments: List[Expression]) extends Expression
@@ -27,13 +24,9 @@ case class FunctionCallExpr(function: Expression, arguments: List[Expression]) e
 sealed trait Statement extends ASTNode
 
 case class LetStmt(identifier: IdentifierExpr, value: Expression) extends Statement
-
 case class ReturnStmt(value: Expression) extends Statement
-
 case class ExpressionStmt(expression: Expression) extends Statement
-
 case class BlockStmt(statements: List[Statement]) extends Statement
-
 case class IfStmt(
                    condition: Expression,
                    consequence: BlockStmt,
